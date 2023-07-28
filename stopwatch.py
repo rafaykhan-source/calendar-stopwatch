c317eabdd637e51df407939b8e8868b74fa76645
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2023-07-28 15:26:21 -0400

    Add boilerplate to stopwatch.py

---
+++ stopwatch.py
@@ -0,0 +1,13 @@
+class StopWatch():
+    def __init__() -> None:
+        return
+    
+    def __str__() -> str:
+        return ""
+
+def main() -> None:
+    # TODO: Add Unit Testing
+    return
+    
+if __name__ == "__main__":
+    main()
