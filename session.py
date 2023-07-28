29d7a48c4ec5d03d58322bb6ade1f2f8f4c8b553
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2023-07-28 16:39:43 -0400

    Add session.py, boilerplate, and docstring

---
+++ session.py
@@ -0,0 +1,36 @@
+"""A class representing a Session.
+
+This module defines the Session ADT, which stores information and supports 
+operations relevant to logic associated utilizing a StopWatch and information
+of why a User needs the use the stopwatch.
+
+Typical usage example:
+
+  session = Session()
+  session.begin()
+  session.end()
+"""
+
+class Session:
+    def __init__(self) -> None:
+        return
+
+    def begin(self) -> None:
+        # TODO: Implement a Begin Functionality
+        return
+
+    def end(self) -> None:
+        # TODO: Implement a End Functionality
+        return
+
+    def __str__(self) -> str:
+        return ""
+
+
+def main() -> None:
+    # TODO: Add Unit Testing
+    return
+
+
+if __name__ == "__main__":
+    main()
