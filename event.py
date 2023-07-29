9fb0ed35740f8a46cb0aa19240831bee08dbef29
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2023-07-29 18:42:08 -0400

    Add create event from session method

--- event.py
+++ event.py
@@ -7,19 +7,38 @@ Typical usage example:
   event = Event()
 """
 
+import time
 from datetime import datetime
+from session import Session
+
 
 class Event:
     def __init__(
         self,
-        summary: str,
-        description: str,
-        start_date: datetime,
-        end_date: datetime,
+        summary: str = "",
+        description: str = "",
+        start_date: datetime = None,
+        end_date: datetime = None,
     ) -> None:
         self.event = {
             "summary": f"{summary}",
             "description": f"{description}",
+            "start": {
+                "dateTime": f"{start_date.isoformat() if start_date else ''}",
+                "timeZone": "America/New_York",
+            },
+            "end": {
+                "dateTime": f"{end_date.isoformat() if end_date else ''}",
+                "timeZone": "America/New_York",
+            },
+        }
+        return
+
+    def create_event_from_session(self, session: Session) -> None:
+        start_date, end_date = session.get_session_time_range()
+        self.event = {
+            "summary": f"{session.title}",
+            "description": f"{session.description}",
             "start": {
                 "dateTime": f"{start_date.isoformat()}",
                 "timeZone": "America/New_York",
@@ -29,7 +48,6 @@ class Event:
                 "timeZone": "America/New_York",
             },
         }
-        return
 
     def get_event_dictionary(self) -> dict:
         """Returns the Event Information as a dictionary.
@@ -44,12 +62,23 @@ class Event:
 
 
 def main() -> None:
-    event = Event(
-        summary="Marshmallow Development",
+    session = Session(
+        title="Marshmallow Development",
         description="Intensive Refactoring",
-        start_date=datetime(year=2023, month=7, day=29, hour=16),
-        end_date=datetime(year=2023, month=7, day=29, hour=18),
     )
+
+    session.begin()
+    time.sleep(5)
+    session.end()
+
+    event = Event()
+    event.create_event_from_session(session)
+    # event = Event(
+    #     summary="Marshmallow Development",
+    #     description="Intensive Refactoring",
+    #     start_date=datetime(year=2023, month=7, day=29, hour=16),
+    #     end_date=datetime(year=2023, month=7, day=29, hour=18),
+    # )
     event_info = event.get_event_dictionary()
     for key, value in event_info.items():
         print(key, value)
