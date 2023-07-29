0e0cb03ca9c2c7889d9c4616c86b4cdd639511bc
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2023-07-28 22:32:48 -0400

    Add error logic to Session ADT

--- session.py
+++ session.py
@@ -11,6 +11,7 @@ Typical usage example:
   session.end()
 """
 
+
 class Session:
     def __init__(self, title: str, description: str) -> None:
         self.title = title
@@ -20,11 +21,20 @@ class Session:
         return
 
     def begin(self) -> None:
+        if self.__began:
+            print("Error: Cannot begin Session that has already begun.")
+            return
         self.__began = True
         # TODO: Implement a Begin Functionality
         return
 
     def end(self) -> None:
+        if not self.__began:
+            print("Error: Cannot end Session that has not yet begun.")
+            return
+        if self.__ended:
+            print("Error: Cannot end Session that has already ended.")
+            return
         self.__ended = True
         # TODO: Implement a End Functionality
         return
