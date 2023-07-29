1d88da26cc48429e260db7f1d40926c4a3a4c8b0
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2023-07-29 19:53:36 -0400

    Add title and description args

--- run.py
+++ run.py
@@ -1,12 +1,24 @@
+import argparse
 import calendar_interactor as cal
 from event import Event
 from session import Session
 
 
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog="google-stopwatch",
+        description="A stopwatch that logs your session on google calendar."
+    )
+    parser.add_argument("--title", help="name of the session (and google calendar event)")
+    parser.add_argument("--description", help="description of the session (and google calendar event)")
+    return parser.parse_args()
+
+
 def main() -> None:
+    args = get_args()
     session = Session(
-        title="Marshmallow Development",
-        description="Intensive Refactoring",
+        title=args.title,
+        description=args.description,
     )
 
     session.begin()
