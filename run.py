6f0eba2003fc41d4a708bd7d1c959fb824df8f42
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2023-07-29 18:44:21 -0400

    Add imports and run.py logic

--- run.py
+++ run.py
@@ -1,4 +1,23 @@
+import calendar_interactor as cal
+from event import Event
+from session import Session
+
+
 def main() -> None:
+    session = Session(
+        title="Marshmallow Development",
+        description="Intensive Refactoring",
+    )
+
+    session.begin()
+    input("Press Enter to Stop: ")
+    session.end()
+
+    event = Event()
+    event.create_event_from_session(session)
+
+    cal.add_event(event)
+
     return
 
 
