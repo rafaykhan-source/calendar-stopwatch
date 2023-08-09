7d7203dc795fd22f83135333adb2b5b18f51bfad
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2023-08-08 20:06:09 -0400

    Add get_logging_config dictionary

--- config/settings.py
+++ config/settings.py
@@ -2,10 +2,10 @@ import yaml
 
 
 def get_logging_config() -> dict:
-    """_summary_
+    """Returns logging config dictionary from logging_config.yml.
 
     Returns:
-        dict: _description_
+        dict: Logging config dictionary.
     """
     with open("config/logging_config.yml", encoding="utf-8", mode="r") as file:
         config = yaml.safe_load(file)
