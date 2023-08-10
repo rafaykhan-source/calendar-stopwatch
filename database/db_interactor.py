0461228ae9eedebf90004f6667665101617fc5a8
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2023-08-09 21:06:48 -0400

    Add logging

--- database/db_interactor.py
+++ database/db_interactor.py
@@ -3,12 +3,15 @@
 This module should hold several functions related to data production
 and consumption.
 """
+
+import logging
 import sqlite3
 
+# logger = logging.getLogger("database")
 
 def main() -> None:
     """Unit Testing."""
-    
+
     return
 
 if __name__ == "__main__":
