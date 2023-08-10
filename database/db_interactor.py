bfde6baa8cc367c3a159134f2a704355dcc9838c
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2023-08-09 21:04:08 -0400

    Add db_interactor boilerplate

---
+++ database/db_interactor.py
@@ -0,0 +1,15 @@
+"""This module is responsible for handling database interactions.
+
+This module should hold several functions related to data production
+and consumption.
+"""
+import sqlite3
+
+
+def main() -> None:
+    """Unit Testing."""
+    
+    return
+
+if __name__ == "__main__":
+    main()
