0c17838b9ff8bcfcd386c7381bc11ed586acd2c6
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2023-08-14 17:27:56 -0400

    Update bash function

--- stopwatch.sh
+++ stopwatch.sh
@@ -2,7 +2,7 @@
 stopwatch() {
     cd
     source .virtualenvs/google-stopwatch/bin/activate
-    cd Projects/google-stopwatch/src/
+    cd Projects/google-stopwatch/google-stopwatch/
     ./run.py "$1" -d "$2"
     cd
     deactivate
