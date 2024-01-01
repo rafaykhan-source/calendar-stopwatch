62bc15c5d0c8c39c5fa393cf1d64b3e75e889be9
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2023-12-31 19:57:53 -0500

    Change directory name

--- stopwatch.sh
+++ stopwatch.sh
@@ -1,7 +1,7 @@
 # Custom Scripts:
 stopwatch() {
     cd
-    source .virtualenvs/google-stopwatch/bin/activate
+    source .virtualenvs/calendar-stopwatch/bin/activate
     cd Projects/calendar-stopwatch/src/
     ./run.py "$1" -d "$2"
     cd
