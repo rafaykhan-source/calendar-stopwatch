c05ccd4242c56e669dda92eb037930df033a2e71
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2024-08-24 22:19:55 -0400

    chore: update stopwatch.sh

--- stopwatch.sh
+++ stopwatch.sh
@@ -1,9 +1,10 @@
-# Custom Scripts:
-stopwatch() {
-    cd
-    source .virtualenvs/calendar-stopwatch/bin/activate
-    cd Projects/calendar-stopwatch/src/
-    ./run.py "$1" -d "$2"
-    cd
+# Custom Functions:
+
+function stopwatch() {
+    prev=$(pwd)
+    source ~/.virtualenvs/calendar-stopwatch/bin/activate
+    cd ~/Projects/calendar-stopwatch/src/
+    python run.py "$1" -d "$2"
+    cd $prev
     deactivate
 }
